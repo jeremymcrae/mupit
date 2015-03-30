71c67d2aa1f3dba7907adf887fa5eedaf4aa0329
Jeremy McRae <jm33@sanger.ac.uk>
2015-03-30 16:27:40 +0100

    now include phenotype similarity when combined tests. Tidied paths and include DDG2P status

--- R/combine_analyses.R
+++ R/combine_analyses.R
@@ -27,6 +27,7 @@ combine_enrichment_and_clustering <- function(enrichment_path, clustering_path) 
     # read in p values from clustering analysis, only for genes with >1 mutation
     clust = read.table(clustering_path, header=TRUE, sep="\t")
     clust = reshape::cast(clust, gene_id ~ mutation_category, value="probability", mean)
+    names(clust)[2:3] = paste("p_", names(clust)[2:3], "_clust", sep="")
     
     # read in p values from mupit analyses
     enriched = read.table(enrichment_path, header=TRUE, sep="\t")
@@ -39,17 +40,20 @@ combine_enrichment_and_clustering <- function(enrichment_path, clustering_path) 
     # We don't expect the loss-of-function de novos to be clustered, so we don't
     # use those. This way we can later get the minimum of the loss-of-function
     # tests, and the minimum of the combined P values.
-    p_values = merged[, c("synonymous", "missense", "p_func", "p_synonymous")]
+    p_values = merged[, c("p_missense_clust", "p_func")]
     merged$p_combined = apply(p_values, 1, fishersMethod)
     
     # # we might want to state the correlation between the P values from the
     # # different analyses, to show that the two analyses are independent.
-    # print(cor(-log10(p_values$synonymous), -log10(p_values$p_synonymous), use="complete.obs")^2)
     # print(cor(-log10(p_values$missense), -log10(p_values$p_func), use="complete.obs")^2)
     
     # adjust the P values by false-discovery rate
     num.tests = 18500
     merged$combined_fdr = p.adjust(merged$p_combined, method="BH", n=num.tests)
+    
+    # calculate minimum p value across LoF and func + clustering tests
+    merged$p_min = apply(merged[, c("p_lof", "p_combined")], 1,
+        function(x) ifelse(length(x[!is.na(x)]) > 0, min(x[!is.na(x)]), NA))
     
     return(merged)
 }
@@ -61,30 +65,31 @@ combine_enrichment_and_clustering <- function(enrichment_path, clustering_path) 
 #' @param meta_enrich path to enrichment results for the meta-analysis subset
 #' @param clust path to clustering results for the ddd only subset
 #' @param enrich path to enrichment results for the ddd only subset
+#' @param phenotype path to phenotype similarity testing results
 #' @export
 #'
 #' @return data frame with the columns from all the datasets, as well as minimum
 #'     P values from each subset for each gene, and overall minimum P values for
 #'     each gene.
-combine_tests <- function(meta_clust, meta_enrich, clust, enrich) {
-    # load all the data files in. Previously Matt was checking two different
-    # subsets, one where the undiagnosed individuals were included, and one
-    # where they were excluded. I've swapped to only using the undiagnosed
-    # excluded subset
+combine_tests <- function(meta_clust, meta_enrich, clust, enrich, phenotype) {
+    # load all the data files in
     meta = combine_enrichment_and_clustering(meta_enrich, meta_clust)
     ddd = combine_enrichment_and_clustering(enrich, clust)
     
+    # read in p values from HPO similarity analyses
+    phenotypes = read.table(phenotype, header=TRUE, sep="\t")
+    
+    ddd = merge(ddd, phenotypes, by="hgnc", all.x=TRUE)
+    ddd$p_min_with_pheno = apply(ddd[, c("p_min", "hpo_similarity_p_value")], 1, fishersMethod)
+    
     # need names that are more informative as same across files, add prefix
-    names(ddd) = paste("ddd", names(ddd), sep=".")
-    names(meta) = paste("meta", names(meta), sep=".")
+    names(ddd)[3:length(names(ddd))] = paste("ddd", names(ddd)[3:length(names(ddd))], sep=".")
+    names(meta)[3:length(names(meta))] = paste("meta", names(meta)[3:length(names(meta))], sep=".")
     
     # merge together files, focusing on genes with DNMs in DDD
-    merged = merge(meta, ddd, by.x="meta.hgnc", by.y="ddd.hgnc", all.x=TRUE)
-    
-    # calculate minimum p value across LoF and func + clustering tests for each dataset
-    merged$p_min_ddd = apply(merged[, c("ddd.p_lof", "ddd.p_combined")], 1, min, na.rm=TRUE)
-    merged$p_min_meta = apply(merged[, c("meta.p_lof",  "meta.p_combined")], 1, min, na.rm=TRUE)
-    merged$p_min = apply(merged[, c("p_min_ddd", "p_min_meta")], 1, min, na.rm=TRUE)
+    merged = merge(meta, ddd, by=c("hgnc", "chrom"), all.x=TRUE)
+    merged$p_min = apply(merged[, c("ddd.p_min_with_pheno", "meta.p_min")], 1,
+        function(x) ifelse(length(x[!is.na(x)]) > 0, min(x[!is.na(x)]), NA))
     
     return(merged)
 }
--- scripts/combine_all_tests.R
+++ scripts/combine_all_tests.R
@@ -5,14 +5,31 @@ library(mupit)
 CODE_DIR = "/nfs/users/nfs_j/jm33/apps/mupit"
 RESULTS_DIR = file.path(CODE_DIR, "results")
 
-diagnosed_string = "without_diagnosed"
-prefix = paste("de_novos.ddd_4k.", diagnosed_string, sep="")
+DDG2P_PATH = "/nfs/ddd0/Data/datafreeze/ddd_data_releases/2014-11-04/DDG2P_freeze_with_gencode19_genomic_coordinates_20141118_fixed.txt"
 
-# define the normal de novo analysis result files
-ddd_meta_clust = file.path(RESULTS_DIR, paste(prefix, ".meta-analysis.clustering_results.txt", sep=""))
-ddd_meta_enrich = file.path(RESULTS_DIR, paste(prefix, ".meta-analysis.enrichment_results.txt", sep=""))
-ddd_clust = file.path(RESULTS_DIR, paste(prefix, ".ddd_only.clustering_results.txt", sep=""))
-ddd_enrich = file.path(RESULTS_DIR, paste(prefix, ".ddd_only.enrichment_results.txt", sep=""))
+include_ddg2_status <- function(merged) {
+    # load the DDG2P, so that we can annotated the genes with their DDG2P status
+    ddg2p = read.table(DDG2P_PATH, sep="\t", header=TRUE, stringsAsFactor=FALSE)
+    ddg2p = ddg2p[ddg2p$DDD_category != "Possible DD Gene", ]
+    
+    # annotate each column with DDG2P status
+    merged$in_ddg2p = merged$hgnc %in% ddg2p$gencode_gene_name
+    
+    return(merged)
+}
 
-merged = combine_tests(ddd_meta_clust, ddd_meta_enrich, ddd_clust, ddd_enrich)
-write.table(merged, file=file.path(RESULTS_DIR, paste(prefix, ".all.txt", sep="")), row.names=F, quote=F, sep="\t")
+diagnosed = c("with_diagnosed", "without_diagnosed")
+for (diagnosed_string in diagnosed) {
+    prefix = paste("de_novos.ddd_4k.", diagnosed_string, sep="")
+
+    # define the normal de novo analysis result files
+    meta_clust = file.path(RESULTS_DIR, paste(prefix, ".meta-analysis.clustering_results.txt", sep=""))
+    meta_enrich = file.path(RESULTS_DIR, paste(prefix, ".meta-analysis.enrichment_results.txt", sep=""))
+    clust = file.path(RESULTS_DIR, paste(prefix, ".ddd_only.clustering_results.txt", sep=""))
+    enrich = file.path(RESULTS_DIR, paste(prefix, ".ddd_only.enrichment_results.txt", sep=""))
+    pheno = file.path(RESULTS_DIR, paste(prefix, ".ddd_only.phenotype_similarity_results.txt", sep=""))
+
+    merged = combine_tests(meta_clust, meta_enrich, clust, enrich, pheno)
+    merged = include_ddg2_status(merged)
+    write.table(merged, file=file.path(RESULTS_DIR, paste(prefix, ".all.txt", sep="")), row.names=F, quote=F, sep="\t")
+}
