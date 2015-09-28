6fbb4efe4b5830da7c487bd527de8c8d3352cf7f
Jeremy McRae <jm33@sanger.ac.uk>
2015-09-28 17:09:50 +0100

    added unit tests for loading DDD data

---
+++ tests/testthat/test_loading_ddd_data.R
@@ -0,0 +1,98 @@
+# unit testing for the mupit functions
+
+library(mupit)
+library(testthat)
+
+context("Loading DDD de novos checks")
+
+test_that("standardise_ddd_de_novos output is correct", {
+    
+    # define a text file for loading, and write it to a file
+    text = c("person_stable_id\tsex\tchrom\tpos\tref\talt\tsymbol\tconsequence\tvar_type",
+        "person_1\tF\t1\t1000\tA\tG\tGENE1\tmissense_variant\tDENOVO-SNP",
+        "person_2\tM\t1\t1010\tC\tGGG\tGENE2\tframeshift_variant\tDENOVO-INDEL",
+        "person_3\tF\tX\t2000\tCCC\tG\tGENE3\tframeshift_variant\tDENOVO-INDEL")
+    path = tempfile()
+    writeLines(text, path, sep="\n")
+    
+    # define the expected output
+    output = read.table(header=TRUE, text="
+        person_id sex chrom start_pos end_pos ref_allele alt_allele hgnc  consequence       study_code  publication_doi study_phenotype type
+        person_1  F   1     1000      1000    A          G          GENE1 missense_variant  ddd_unpublished  NA  developmental_disorders snv
+        person_2  M   1     1010      1010    C          GGG        GENE2 frameshift_variant ddd_unpublished  NA  developmental_disorders indel
+        person_3  F   X     2000      2002    CCC        G          GENE3 frameshift_variant ddd_unpublished  NA  developmental_disorders indel",
+        colClasses=c("character", "character", "character", "numeric", "numeric", "character", "character", "character", "character", "character", "logical", "character", "character"))
+    
+    expect_equal(standardise_ddd_de_novos(path), output)
+    unlink(path)
+})
+
+test_that("get_ddd_de_novos output is correct", {
+    
+    # define a text file for loading, and write it to a file
+    text = c("person_stable_id\tsex\tchrom\tpos\tref\talt\tsymbol\tconsequence\tvar_type",
+        "person_1\tF\t1\t1000\tA\tG\tGENE1\tmissense_variant\tDENOVO-SNP",
+        "person_2\tM\t1\t1010\tC\tGGG\tGENE2\tframeshift_variant\tDENOVO-INDEL",
+        "person_3\tF\tX\t2000\tCCC\tG\tGENE3\tframeshift_variant\tDENOVO-INDEL")
+    path = tempfile()
+    writeLines(text, path, sep="\n")
+    
+    # define the expected output
+    output = read.table(header=TRUE, text="
+        person_id sex chrom start_pos end_pos ref_allele alt_allele hgnc  consequence       study_code  publication_doi study_phenotype type
+        person_1  F   1     1000      1000    A          G          GENE1 missense_variant  ddd_unpublished  NA  developmental_disorders snv
+        person_2  M   1     1010      1010    C          GGG        GENE2 frameshift_variant ddd_unpublished  NA  developmental_disorders indel
+        person_3  F   X     2000      2002    CCC        G          GENE3 frameshift_variant ddd_unpublished  NA  developmental_disorders indel",
+        colClasses=c("character", "character", "character", "numeric", "numeric", "character", "character", "character", "character", "character", "logical", "character", "character"))
+    
+    expect_equal(get_ddd_de_novos(path), output)
+    unlink(path)
+})
+
+test_that("get_ddd_de_novos output is correct when removing the diagnosed set", {
+    
+    # define a text file for loading, and write it to a file
+    text = c("person_stable_id\tsex\tchrom\tpos\tref\talt\tsymbol\tconsequence\tvar_type",
+        "person_1\tF\t1\t1000\tA\tG\tGENE1\tmissense_variant\tDENOVO-SNP",
+        "person_2\tM\t1\t1010\tC\tGGG\tGENE2\tframeshift_variant\tDENOVO-INDEL",
+        "person_3\tF\tX\t2000\tCCC\tG\tGENE3\tframeshift_variant\tDENOVO-INDEL")
+    path = tempfile()
+    writeLines(text, path, sep="\n")
+    
+    exclude = data.frame("person_id"=c("person_1", "person_2"), "sex"=c("F", "M"))
+    
+    # define the expected output
+    output = read.table(header=TRUE, text="
+        person_id sex chrom start_pos end_pos ref_allele alt_allele hgnc  consequence       study_code  publication_doi study_phenotype type
+        person_3  F   X     2000      2002    CCC        G          GENE3 frameshift_variant ddd_unpublished  NA  developmental_disorders indel",
+        colClasses=c("character", "character", "character", "numeric", "numeric", "character", "character", "character", "character", "character", "logical", "character", "character"))
+    
+    # set the row names for the output, taking into account the saples that have
+    # been removed.
+    attr(output, "row.names") = as.integer(3)
+    
+    expect_equal(get_ddd_de_novos(path, diagnosed=exclude), output)
+    unlink(path)
+})
+
+test_that("get_ddd_de_novos output is correct when removing the diagnosed set", {
+    
+    # define a text file for loading, and write it to a file
+    text = c("person_stable_id\tsex\tchrom\tpos\tref\talt\tsymbol\tconsequence\tvar_type",
+        "person_1\tF\t1\t1000\tA\tG\tGENE1\tmissense_variant\tDENOVO-SNP",
+        "person_2\tM\t1\t1010\tC\tGGG\tGENE2\tframeshift_variant\tDENOVO-INDEL",
+        "person_3\tF\tX\t2000\tCCC\tG\tGENE3\tframeshift_variant\tDENOVO-INDEL")
+    path = tempfile()
+    writeLines(text, path, sep="\n")
+    
+    include = c("person_1")
+    
+    # define the expected output
+    output = read.table(header=TRUE, text="
+        person_id sex chrom start_pos end_pos ref_allele alt_allele hgnc  consequence       study_code  publication_doi study_phenotype type
+        person_1  F   1     1000      1000    A          G          GENE1 missense_variant  ddd_unpublished  NA  developmental_disorders snv",
+        colClasses=c("character", "character", "character", "numeric", "numeric", "character", "character", "character", "character", "character", "logical", "character", "character"))
+    
+    expect_equal(get_ddd_de_novos(path, subset=include), output)
+    unlink(path)
+})
