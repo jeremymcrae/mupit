2da50df5e0dc83be329769155b174301360f5ea6
Jeremy McRae <jm33@sanger.ac.uk>
2015-09-28 13:29:11 +0100

    added unit tests for combining statistics from different tests

--- R/combine_analyses.R
+++ R/combine_analyses.R
@@ -18,42 +18,27 @@ fishersMethod <- function(x) {
 
 #' combine P values from enrichment and clustering tests into a single P value
 #'
-#' @param enrichment_path path to enrichment testing results
-#' @param clustering_path path to clustering testing results
+#' @param enriched dataframe of de novo enrichment results
+#' @param clust dataframe of de novo clustering results
+#' @param num_tests number of tests to correct for multiple testing
 #' @export
 #'
 #' @return a merged dataset where the P values have been combined
-combine_enrichment_and_clustering <- function(enrichment_path, clustering_path) {
+combine_enrichment_and_clustering <- function(enriched, clust, num_tests=18500) {
     # read in p values from clustering analysis, only for genes with >1 mutation
-    clust = read.table(clustering_path, header=TRUE, sep="\t")
     clust = reshape::cast(clust, gene_id ~ mutation_category, value="probability", mean)
     names(clust)[2:3] = paste("p_", names(clust)[2:3], "_clust", sep="")
-    
-    # read in p values from mupit analyses
-    enriched = read.table(enrichment_path, header=TRUE, sep="\t")
     
     # merge the datasets
     merged = merge(enriched, clust, by.x="hgnc", by.y="gene_id", all.x=TRUE)
     
-    # calculate a combined p-value for each gene, using the synonymous and
-    # missense categories from each of the enrichment and clustering analyses.
-    # We don't expect the loss-of-function de novos to be clustered, so we don't
-    # use those. This way we can later get the minimum of the loss-of-function
-    # tests, and the minimum of the combined P values.
-    p_values = merged[, c("p_missense_clust", "p_func")]
+    # calculate a combined p-value for each gene. We don't expect the
+    # loss-of-function de novos to be clustered, so we don't use that.
+    p_values = merged[, c("enrichment_p_value", "p_missense_clust")]
     merged$p_combined = apply(p_values, 1, fishersMethod)
     
-    # # we might want to state the correlation between the P values from the
-    # # different analyses, to show that the two analyses are independent.
-    # print(cor(-log10(p_values$missense), -log10(p_values$p_func), use="complete.obs")^2)
-    
     # adjust the P values by false-discovery rate
-    num.tests = 18500
-    merged$combined_fdr = p.adjust(merged$p_combined, method="BH", n=num.tests)
-    
-    # calculate minimum p value across LoF and func + clustering tests
-    merged$p_min = apply(merged[, c("p_lof", "p_combined")], 1,
-        function(x) ifelse(length(x[!is.na(x)]) > 0, min(x[!is.na(x)]), NA))
+    merged$combined_fdr = p.adjust(merged$p_combined, method="BH", n=num_tests)
     
     return(merged)
 }
@@ -72,7 +57,13 @@ combine_enrichment_and_clustering <- function(enrichment_path, clustering_path) 
 #'     P values from each subset for each gene, and overall minimum P values for
 #'     each gene.
 combine_tests <- function(meta_clust, meta_enrich, clust, enrich, phenotype) {
+    
     # load all the data files in
+    clust = read.table(clust, header=TRUE, sep="\t")
+    enrich = read.table(enrich, header=TRUE, sep="\t")
+    meta_clust = read.table(meta_clust, header=TRUE, sep="\t")
+    meta_enrich = read.table(meta_enrich, header=TRUE, sep="\t")
+    
     meta = combine_enrichment_and_clustering(meta_enrich, meta_clust)
     ddd = combine_enrichment_and_clustering(enrich, clust)
     
@@ -80,7 +71,7 @@ combine_tests <- function(meta_clust, meta_enrich, clust, enrich, phenotype) {
     phenotypes = read.table(phenotype, header=TRUE, sep="\t")
     
     ddd = merge(ddd, phenotypes, by="hgnc", all.x=TRUE)
-    ddd$p_min_with_pheno = apply(ddd[, c("p_min", "hpo_similarity_p_value")], 1, fishersMethod)
+    ddd$p_min_with_pheno = apply(ddd[, c("p_combined", "hpo_similarity_p_value")], 1, fishersMethod)
     
     # need names that are more informative as same across files, add prefix
     names(ddd)[3:length(names(ddd))] = paste("ddd", names(ddd)[3:length(names(ddd))], sep=".")
@@ -88,7 +79,7 @@ combine_tests <- function(meta_clust, meta_enrich, clust, enrich, phenotype) {
     
     # merge together files, focusing on genes with DNMs in DDD
     merged = merge(meta, ddd, by=c("hgnc", "chrom"), all.x=TRUE)
-    merged$p_min = apply(merged[, c("ddd.p_min_with_pheno", "meta.p_min")], 1,
+    merged$p_min = apply(merged[, c("ddd.p_combined", "ddd.p_min_with_pheno", "meta.p_combined")], 1,
         function(x) ifelse(length(x[!is.na(x)]) > 0, min(x[!is.na(x)]), NA))
     
     return(merged)
---
+++ tests/testthat/test_combining_statistics.R
@@ -0,0 +1,66 @@
+# unit testing for the mupit functions
+
+library(mupit)
+library(testthat)
+library(jsonlite)
+
+context("combining analyses checks")
+
+test_that("fishersMethod is correct for simple vector", {
+    p_values = c(0.01, 0.001)
+    expect_equal(fishersMethod(p_values), 0.000125129254)
+})
+
+test_that("fishersMethod is correct for single value", {
+    p_values = c(0.01)
+    expect_equal(fishersMethod(p_values), 0.01)
+})
+
+test_that("fishersMethod is correct for vector with NA", {
+    p_values = c(0.01, NA)
+    expect_equal(fishersMethod(p_values), 0.01)
+})
+
+test_that("fishersMethod is correct for vector with NA", {
+    p_values = c(0.01, 0.001, NA)
+    expect_equal(fishersMethod(p_values), 0.000125129254)
+})
+
+test_that("fishersMethod is correct without any p-values", {
+    p_values = c(NA, NA)
+    expect_equal(fishersMethod(p_values), NA)
+})
+
+test_that("fishersMethod is correct for vector with zero", {
+    p_values = c(0, 0.01)
+    expect_equal(fishersMethod(p_values), 0)
+})
+
+test_that("combine_enrichment_and_clustering is correct for small table", {
+    enriched = read.table(header=TRUE, text="
+        hgnc  enrichment_p_value
+        GENE1 0.01
+        GENE2 0.0001")
+    
+    clust = read.table(header=TRUE, text="
+        gene_id mutation_category probability
+        GENE1   missense          0.1
+        GENE1   nonsense          0.1
+        GENE2   missense          0.001
+        GENE2   nonsense          0.001")
+    
+    output = read.table(header=TRUE, text="
+    hgnc  enrichment_p_value p_missense_clust p_nonsense_clust p_combined    combined_fdr
+    GENE1 1e-02              0.100            0.100            7.9077553e-03 1.000000000
+    GENE2 1e-04              0.001            0.001            1.7118110e-06 0.031668477")
+    
+    expect_equal(combine_enrichment_and_clustering(enriched, clust), output)
+    
+    # and check that the values are correct when we alter the num_tests variable
+    output = read.table(header=TRUE, text="
+    hgnc  enrichment_p_value p_missense_clust p_nonsense_clust p_combined   combined_fdr
+    GENE1 1e-02              0.100            0.100            7.9077553e-03 0.015815510557
+    GENE2 1e-04              0.001            0.001            1.7118110e-06 6.84723826e-06")
+    
+    expect_equal(combine_enrichment_and_clustering(enriched, clust, num_tests=4), output)
+})
